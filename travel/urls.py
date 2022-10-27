9cab22f17ab0404d1edc2e3c9ef7538313766bd1
Inst1nct007 <94550979+Inst1nct007@users.noreply.github.com>
2022-10-28 01:56:36 +0530

    UI and backend complete

--- travel/urls.py
+++ travel/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from .views import PlaceListView, details
 
 urlpatterns = [
-    path('', views.home, name='travel-home'),
+    path('', PlaceListView.as_view(), name='travel-home'),
+    path('place/<int:pk>/', details, name='place-details'),
     path('international/', views.international, name='travel-international'),
     path('domestic/', views.domestic, name='travel-domestic'),
+    path('about/', views.about, name='travel-about'),
 ]
--- travel/views.py
+++ travel/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Place
+from .models import Place, PlacesToVisit
+from django.views.generic import ListView, DetailView
 
 
 def home(request):
@@ -8,6 +9,21 @@ def home(request):
         'places': Place.objects.all()
     }
     return render(request, 'travel/home.html', context)
+
+
+class PlaceListView(ListView):
+    model = Place
+    template_name = 'travel/home.html'
+    context_object_name = 'places'
+    ordering = ['name']
+
+
+def details(request, pk):
+    context = {
+        'title': 'Details',
+        'places_to_visit': PlacesToVisit.objects.filter(place=Place.objects.filter(id=pk)[0])
+    }
+    return render(request, 'travel/place_details.html', context)
 
 
 def international(request):
@@ -24,3 +40,10 @@ def domestic(request):
         'places': Place.objects.filter(category='DM')
     }
     return render(request, 'travel/places.html', context)
+
+
+def about(request):
+    context = {
+        'title': 'About',
+    }
+    return render(request, 'travel/about.html', context)
